91f5e26fa456a5f391548c61db719277f0ab187e
Brayam1993 <argofrog@live.com.mx>
2022-03-28 19:35:24 -0600

    add controller find all users

--- mysql_src/controllers/user.controller.js
+++ mysql_src/controllers/user.controller.js
@@ -14,3 +14,13 @@ exports.create = function (req, res) {
         });
     };
 };
+
+exports.findAll = function(req, res) {
+    User.findAll(function(err, user) {
+        console.log('controller')
+        if(err)
+        res.send(err);
+        console.log('res', user);
+        res.send(user);
+    });
+};
