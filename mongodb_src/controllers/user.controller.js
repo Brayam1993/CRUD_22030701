bff1ff384d59a35db50432d0c2f5f55f4076a500
Brayam1993 <argofrog@live.com.mx>
2022-04-09 20:47:28 -0500

    eslint mongodb fix files js

--- mongodb_src/controllers/user.controller.js
+++ mongodb_src/controllers/user.controller.js
@@ -1,7 +1,8 @@
-user_mgdb = require('../models/user.model.js');
+/* eslint-disable func-names */
+const UserMgdb = require('../models/user.model');
 
 exports.index = function (req, res) {
-  user_mgdb.get((err, userMgdbOtro) => {
+  UserMgdb.get((err, userMgdbOtro) => {
     if (err) {
       res.json({
         status: 'error',
@@ -18,16 +19,14 @@ exports.index = function (req, res) {
 
 // Post new user in mongodb
 exports.new = function (req, res) {
-  const mgdb = new user_mgdb();
+  const mgdb = new UserMgdb();
   mgdb.username = req.body.username ? req.body.username : mgdb.username;
   mgdb.fullname = req.body.fullname;
   mgdb.age = req.body.age;
   mgdb.password = req.body.password;
 
   // save the usermongodb an check for errors
-  mgdb.save((err) => {
-    //
-    //
+  mgdb.save(() => {
     res.json({
       message: 'New User Mongodb created!',
       data: mgdb,
@@ -36,19 +35,18 @@ exports.new = function (req, res) {
 };
 
 exports.update = function (req, res) {
-  user_mgdb.findById(req.params.mgdb_id, (err, book) => {
+  UserMgdb.findById(req.params.mgdb_id, (err) => {
     if (err) res.send(err);
-    user_mgdb.username = req.body.username ? req.body.username : user_mgdb.username;
-    user_mgdb.fullname = req.body.fullname;
-    user_mgdb.age = req.body.age;
-    user_mgdb.password = req.body.password;
+    UserMgdb.username = req.body.username ? req.body.username : UserMgdb.username;
+    UserMgdb.fullname = req.body.fullname;
+    UserMgdb.age = req.body.age;
+    UserMgdb.password = req.body.password;
 
-    // save the mongo user and check for errors
-    mgdb.save((err) => {
+    UserMgdb.save(() => {
       if (err) res.json(err);
       res.json({
         message: 'Mongo User Updated',
-        data: mgdb,
+        data: UserMgdb,
       });
     });
   });
@@ -56,9 +54,9 @@ exports.update = function (req, res) {
 
 // Delete a user in mongodb
 exports.delete = function (req, res) {
-  user_mgdb.deleteOne({
+  UserMgdb.deleteOne({
     _id: req.params.book_id,
-  }, (err, userMgdbOtroMas) => {
+  }, (err) => {
     if (err) res.send(err);
     res.json({
       status: 'success',
