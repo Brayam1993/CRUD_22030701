996b22e2cec6ce1891eb150ebf681e9f24e8eb21
Jonatan Carrillo <tbkjfc@gmail.com>
2022-04-28 22:07:27 -0500

    Added mock samples

--- mysql/src/controllers/__tests__/user.controller.test.js
+++ mysql/src/controllers/__tests__/user.controller.test.js
@@ -1,8 +1,23 @@
 const { create } = require('../user.controller');
+// const create = jest.fn();
+var httpMocks = require('node-mocks-http');
+
+jest.mock('../user.controller', () => ({
+  res: jest.fn(),
+}));
 
 describe('user controller create', () => {
-  it('it should be a function', () => {
-    // actual test
+  it('should be a function', () => {
+    // actual test 1
     expect(typeof (create)).toBe('function');
+  });
+
+  it('should use 2 params', () => {
+    // actual test 1
+    const request = httpMocks.createRequest();
+    const response = httpMocks.createResponse();
+
+    create(request, response);
+    expect(create).toHaveBeenCalledWith(123, 'lalal');
   });
 });
